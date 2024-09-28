c80df4220ed55d93f7173737b56dbc1e8b17f2a8
Diego Marques <diego.marques@millenniumbcp.pt>
2024-09-28 11:04:36 +0100

    move to azure

--- scripts/remove_replicas.sh
+++ scripts/remove_replicas.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 flux suspend kustomization -n flux-system apps  
-kubectl scale deploy hello-kubernetes-hello-kubernetes --replicas=0
+kubectl scale deploy hello-kubernetes-hello-kubernetes --replicas=0 -n hello-kubernetes
